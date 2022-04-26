d22fc805bb7c2555c1039e859278a8b4e31c7f7b
Jessica <jessica1329b@gmail.com>
2022-04-26 12:37:37 -0400

    javascript variables

--- app.js
+++ app.js
@@ -4,7 +4,37 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jessica";
+let lastName = "Edmonds";
+
+var myAge = 33;
+let isDone;
 
 // Exercise 2
+let fullName = firstName + -- + lastName; 
+let templateFullName = '${firstName} ${lastName}';
+console.log("fullName: ", fuliName);
+console.log("templateFullName:", templateFullName)
 
 // Exercise 3
+let myStory;
+let city = "Tallahassee"
+let pastime = "coding, running, reading, and hanging with friends"
+
+myStory =
+ "My name is " + 
+ fullName +
+  ",/n " +
+   "I live in" +
+    city +
+     "/n" +
+     "I like " +
+     pastime +
+     ",";
+
+     console.log(myStory);
+
+     myStory = "my name is ${fullName},
+      I live in ${city}. I like ${pastime}';
+
+console.log{myStory};
